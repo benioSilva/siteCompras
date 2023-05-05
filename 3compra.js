dd4db1efd7c736a12afa3f90dd3bb8c0a93c5f6c
benioSilva <beniosilva10000@gmail.com>
2023-05-05 13:40:40 -0300

    finalizando toda base javaScript part1

--- 3compra.js
+++ 3compra.js
@@ -5,9 +5,32 @@ function getElementCadastro1Storage() {
     console.log(cadastroProdutosObjeto)
     return cadastroProdutosObjeto
 }
+function getElementCompra3Storage(){
+    const compras3Storage = localStorage.getItem("listaCompra3")
+    const compras3Objeto = JSON.parse(compras3Storage)
+    return compras3Objeto
+}
+
 console.log(getElementCadastro1Storage())
 
-
+function getElementNome3 (){
+    return document.getElementById("nome3")
+}
+function getElementEmail3 (){
+    return document.getElementById("email3")
+}
+function getElementIdade3 () {
+    return document.getElementById("idade3")
+}
+function getElementProduto3 (){
+    return document.getElementById("produtos3")
+}
+function getElementValor3 () {
+    return document.getElementById("valor3")
+}
+function getElementBotao3 () {
+    return document.getElementById("botao3")
+}
 
 loadSelectProdutos()
 
@@ -24,3 +47,32 @@ function onChangeProdutos(){
     document.getElementById("valor3").value = getElementCadastro1Storage()[produtosIndex].Preco1
 }
 console.log(document.getElementById("produtos3").value)
+
+
+getElementBotao3().addEventListener('click', function(event){
+    event.preventDefault();
+    
+
+    let valorCompra3 = {
+        nome3: getElementNome3().value,
+        email3: getElementEmail3().value,
+        idade3: getElementIdade3().value,
+        produto3: getElementCadastro1Storage()[getElementProduto3().value].Nome1,
+        valor3: getElementCadastro1Storage()[getElementProduto3().value].Preco1,
+        status3: "Pendente"
+    };
+    if (getElementCompra3Storage()==null) {
+        const lista3 = [valorCompra3]
+        localStorage.setItem("listaCompra3", JSON.stringify(lista3))
+    } else {
+        const list3 = getElementCompra3Storage()
+        console.log(list3)
+        list3.push(valorCompra3)
+        console.log(list3)
+        localStorage.setItem("listaCompra3", JSON.stringify(list3))
+    }
+console.log(valorCompra3)
+getElementNome3().value = ""
+getElementEmail3().value = ""
+getElementIdade3().value = ""
+})
